97b4f45cca32662705f0c46f1a7907a258b94ab4
yunxiao.zhu <yunxiao.zhu@sh-kelai.com>
2019-09-09 10:35:23 +0800

    修改 jest测试样例

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     node: true
   },
   extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
+  //extends: ["@vue/prettier", "@vue/typescript"],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
