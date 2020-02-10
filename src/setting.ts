d4af7616a4a28d2ec4f413053d427dd7847954d9
xxbld <1024920618@qq.com>
2020-02-10 16:01:39 +0800

    自动提取主题配置

--- src/setting.ts
+++ src/setting.ts
@@ -1,3 +1,57 @@
+interface ITheme {
+  title: string;
+  name: string;
+  logo: any;
+  logoMini: any;
+  backgroundImage?: any;
+  preview?: any;
+}
+function getThemePng(path: string, match: string, needDefault = false) {
+  let png: any = undefined
+  const baseUrl = process.env.BASE_URL
+  const subPath = path.replace('./', '').replace('index.scss', `img/${match}`)
+  try {
+    png = require('./assets/style/theme/' + subPath + '.png')
+    // 如果是url 加上baseurl
+  } catch (error) {
+    // default bg preview
+    if (needDefault) {
+      png = `${baseUrl}image/theme/default/${subPath}.png`
+    }
+  }
+  return png
+}
+const keyName = {
+  'bzh': '标准化',
+  'd2': 'd2admin 经典',
+  'violet': '紫罗兰',
+  'line': '简约线条',
+  'star': '流星',
+  'tomorrow-night-blue': 'Tomorrow Night Blue (vsCode)',
+}
+
+function getThemes() {
+  const themes: ITheme[] = []
+  const files = require.context('./assets/style/theme', true, /index.scss$/)
+  files.keys().forEach((path) => {
+    const arr = path.split('/')
+    if (arr.length === 3) {
+      const themeKey = arr[arr.length - 2]
+      themes.push({
+        title: keyName[themeKey],
+        name: themeKey,
+        backgroundImage: getThemePng(path, 'bg'),
+        preview: getThemePng(path, 'preview@2x', true),
+        logo: getThemePng(path, 'logo-all', true),
+        logoMini: getThemePng(path, 'logo-icon-only', true),
+      })
+    }
+  })
+  return themes;
+}
+const themes = getThemes()
+console.log(themes)
+
 export default {
   // 快捷键
   // 支持快捷键 例如 ctrl+shift+s
